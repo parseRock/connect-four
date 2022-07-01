1fdc7e6c3aca0f9b083c41395c649313d9889dfe
parseRock <tony.oriol@outlook.com>
2022-07-01 19:37:36 -0400

    second commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+/*----- constants -----*/
+
+
+
+
+/*----- app's state (variables) -----*/
+// Array of 42 elements...
+// null -> sq avail; 1 or -1 for the players 
+let board;
+let turn; // 1 or -1
+let gameStatus; // null -> game in play; 1/-1 player win; 'T' -> tie
+
+
+
+/*----- cached element references -----*/
+
+
+
+
+
+/*----- event listeners -----*/
+
+
+
+
+
+/*----- functions -----*/
