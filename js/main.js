a403a3ccce3d402853ec24eaf174fe274f941c98
parseRock <tony.oriol@outlook.com>
2022-07-04 09:48:02 -0400

    createDivs

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@
 // Array of 42 elements...
 // null -> sq avail; 1 or -1 for the players 
 let board;
-let turn; // 1 or -1
+let turn; // 1 or -1; 0 for empty space
 let gameStatus; // null -> game in play; 1/-1 player win; 'T' -> tie
 
 
@@ -25,3 +25,25 @@ let gameStatus; // null -> game in play; 1/-1 player win; 'T' -> tie
 
 
 /*----- functions -----*/
+Infinity();
+
+
+function init() {
+    board = [
+        [0, 0, 0, 0, 0, 0, 0,], //column 0
+        [0, 0, 0, 0, 0, 0, 0,], //column 1
+        [0, 0, 0, 0, 0, 0, 0,], //column 2
+        [0, 0, 0, 0, 0, 0, 0,], //column 3
+        [0, 0, 0, 0, 0, 0, 0,], //column 4
+        [0, 0, 0, 0, 0, 0, 0,], //column 5
+        [0, 0, 0, 0, 0, 0, 0,], //column 6
+    ];
+    turn = 1;
+    render();
+}
+
+function render() {
+    board.forEach(function() {
+
+    });
+}
