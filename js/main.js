51d8589bea2f411ab7f0ef36baf050c9e9684f38
parseRock <tony.oriol@outlook.com>
2022-07-04 12:23:37 -0400

    createDivsJs

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 /*----- constants -----*/
-
+const COLOR = {
+    '0': 'white',
+    '1': 'red',
+    '-1': 'yellow'
+};
 
 
 
@@ -13,21 +17,20 @@ let gameStatus; // null -> game in play; 1/-1 player win; 'T' -> tie
 
 
 /*----- cached element references -----*/
-
+const markerEls = [document.querySelectorAll('#markers > div')] ;
 
 
 
 
 /*----- event listeners -----*/
+document.getElementById('markers').addEventListener('click', handleDrop() {
 
+}); 
 
 
 
 
 /*----- functions -----*/
-Infinity();
-
-
 function init() {
     board = [
         [0, 0, 0, 0, 0, 0, 0,], //column 0
@@ -43,7 +46,23 @@ function init() {
 }
 
 function render() {
-    board.forEach(function() {
-
+    // Iterate over the column arrays
+    board.forEach(function(colArr, colIdx) {
+        colArr.forEach(function(cellVal, rowIdx) {
+            const cellEl = document.getElementById(`c${colIdx}r${rowIdx}`);
+            cellEl.style.backgroundColor = COLORS[cellVal];
+        });
     });
+}
+
+// update all impacted state, then call render
+function handleDrop(evt) {
+    const colIdx = markerEls.indexOf(evt.target)
+    if (colIdx === -1) return;
+    const colArr = board[colIdx];
+    if (!colArr.includes(0)) return;
+    const rowIdx = colArr.indexOf(0);
+    colArr[rowIdx] = turn;
+    turn *= -1;
+    render();
 }
